af5772a7f53ccfa9612ae6ccfb6011c0e04aed0c
MichaelNg97 <michaelngteckhua@gmail.com>
2017-01-24 16:19:25 -0500

    Week 3 Employee class

---
+++ src/wjan24/Employee.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wjan24;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ *
+ * @author Michael
+ */
+public class Employee {
+    private String fname, lname;
+    private String sin;
+    private LocalDate dateOfBirth;
+
+    public Employee(String first, String last, String sin, LocalDate dob){
+    
+        fname = first;
+        lname = last;
+        this.sin = sin;
+        setBirthday(dob);
+}
+    
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getSin() {
+        return sin;
+    }
+
+    public LocalDate getBirthday() {
+        return dateOfBirth;
+    }
+    
+/**
+ * This method will return the employee's first and last name
+ * @return 
+ */    
+public String toString(){
+    return fname +" "+lname;
+}    
+
+/**
+ * This method will validate the new Employee is 15-90 years old
+ * @param birthday 
+ */    
+public void setBirthday(LocalDate birthday){
+    LocalDate today = LocalDate.now();
+    int age = Period.between(birthday, today).getYears();
+    
+    if(age>=15 && age<=90)
+        dateOfBirth = birthday;
+    else 
+        throw new IllegalArgumentException("Employees must be 15-90 years old");
+    
+}    
+
+}
